b47a58252bc1e9a816c6a37482c03c9eaad369de
DevMatheusVinicius <matheusvfc96@hotmail.com>
2024-11-19 00:06:32 -0300

    Update agendamento.js

--- public/assets/js/agendamento.js
+++ public/assets/js/agendamento.js
@@ -1,4 +1,3 @@
-// Função para validar o agendamento
 function validarAgendamento(event) {
     const nome = document.getElementById('nome').value;
     const email = document.getElementById('email').value;
@@ -9,16 +8,16 @@ function validarAgendamento(event) {
     const errorMessage = document.getElementById('error-message');
 
     if (!nome || !email || !veiculo || !servico || !data || !hora) {
-        event.preventDefault(); // Impede o envio do formulário
+        event.preventDefault(); 
         errorMessage.textContent = 'Por favor, preencha todos os campos.';
         errorMessage.style.display = 'block';
     } else {
-        // Limpar a mensagem de erro, caso os dados estejam corretos
+        
         errorMessage.style.display = 'none';
     }
 }
 
-// Adicionando evento para validação no formulário
+
 window.onload = function () {
     const agendamentoForm = document.getElementById('agendamento-form');
     
